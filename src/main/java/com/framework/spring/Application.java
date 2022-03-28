bad225cf768d48d935b29bd9b522b4d5d19128b1
udgttl12 <udgttl12@naver.com>
2022-03-28 18:06:29 +0900

    스프링 패키지 만들기

--- src/main/java/com/framework/spring/Application.java
+++ src/main/java/com/framework/spring/Application.java
@@ -1,13 +1,18 @@
 package com.framework.spring;
 
+import com.framework.spring.game.MarioGame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        //SpringApplication.run(Application.class, args);
+        MarioGame game = new MarioGame();
+        GameRunner runner = new GameRnunner(game);
+
+        runner.run();
+    }
 
 }
